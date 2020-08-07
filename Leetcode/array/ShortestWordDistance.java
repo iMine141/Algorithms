97c3fc401d95b31b02de368a2269f960329ba970
wang yimin <846945674@qq.com>
2020-08-07 10:47:03 +0800

    8.7

---
+++ Leetcode/array/ShortestWordDistance.java
@@ -0,0 +1,27 @@
+package Leetcode.array;
+
+/**
+ * @author wym
+ */
+
+/**
+ * 最短单词间距
+ */
+public class ShortestWordDistance {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int idx1 = -1, idx2 = -1, distance = Integer.MAX_VALUE;
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(word1)){
+                idx1 = i;
+                // 第一次写入idx就先不比较
+                if(idx2 != -1) distance = Math.min(distance, idx1 - idx2);
+            }
+            if(words[i].equals(word2)){
+                idx2 = i;
+                // 第一次写入idx就先不比较
+                if(idx1 != -1) distance = Math.min(distance, idx2 - idx1);
+            }
+        }
+        return distance;
+    }
+}
